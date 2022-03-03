76cfcefc3d3238a2c85576e8617a3e98147dd4f6
TaiwanEconomicDataCenter <tedc1@ms12.hinet.net>
2022-03-03 09:09:27 +0800

    Update SELECT_OECD.sql

--- SELECT_OECD.sql
+++ SELECT_OECD.sql
@@ -1,9 +1,21 @@
 USE qnia;
+CREATE view qnia_info AS
 SELECT databank, name, db_table, db_code, desc_e, desc_c, freq, start, last, unit, name_ord, snl, book, form_e, form_c 
-FROM qnia_key
-LIMIT 0,20;
+FROM qnia_key;
+CREATE view qnia_count AS
+SELECT count(*) AS size
+FROM qnia_key;
+
+SELECT * FROM qnia_info LIMIT 0, 20;
+SELECT * FROM qnia_count;
 
 USE mei;
+CREATE view mei_info AS
 SELECT databank, name, db_table, db_code, desc_e, desc_c, freq, start, last, unit, name_ord, snl, book, form_e, form_c 
-FROM mei_key
-LIMIT 20,20;
+FROM mei_key;
+CREATE view mei_count AS
+SELECT count(*) AS size
+FROM mei_key;
+
+SELECT * FROM mei_info LIMIT 20, 20;
+SELECT * FROM mei_count;
