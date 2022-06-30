130055cca191687cbafe71a605ddbd52a8921513
TaiwanEconomicDataCenter <tedc1@ms12.hinet.net>
2022-06-30 18:09:25 +0800

    Update CHECK_DB.py

--- TO_DB/CHECK_DB.py
+++ TO_DB/CHECK_DB.py
@@ -50,12 +50,14 @@ for f in freqlist:
 print('\nComparison:')
 modified = 0
 displayed = 0
+updated = 0
 modified_data = {}
 for f in freqlist:
     modified_data[f] = pd.DataFrame()
 result = pd.concat([result, pd.DataFrame([['Comparison:', 'display '+str(display_num)]])])
 result = pd.concat([result, pd.DataFrame([['BANK', 'NAME', 'FREQ', 'START', 'LAST', 'DESCRIPTION']])])
 result = pd.concat([result, blank])
+freqlist.reverse()
 for f in freqlist:
     for name in freqsample[f]:
         sys.stdout.write("\rChecking...("+str(round((freqsample[f].index(name)+1)*100/len(freqsample[f]), 1))+"%), Name = "+name+"     ")
@@ -69,13 +71,20 @@ for f in freqlist:
             ERROR('Name Not Found in database '+NAME.lower()+'_old: '+name)
         
         old_data = DATA_BASE_t_old[old_item['db_table']][old_item['db_code']]
+        copy_old_data = old_data.copy()
+        old_data = old_data.dropna()
         new_data = DATA_BASE_t[new_item['db_table']][new_item['db_code']]
-        if not old_data.dropna().equals(new_data.dropna()):
-            if displayed <= display_num and len(new_data.dropna()) > len(old_data.dropna()):
+        copy_new_data = new_data.copy()
+        new_data = new_data.loc[new_data.index.isin(old_data.index)]
+        if len(copy_new_data.dropna()) > len(copy_old_data.dropna()):
+            if displayed <= display_num:
                 result = pd.concat([result, pd.DataFrame([['Old', name, f, old_item['start'], old_item['last'], old_item['desc_e']]])])
                 result = pd.concat([result, pd.DataFrame([['New', name, f, new_item['start'], new_item['last'], new_item['desc_e']]])])
-                result = pd.concat([result, pd.DataFrame([['modified:', True]])])
+                result = pd.concat([result, pd.DataFrame([['updated:', True]])])
+                result = pd.concat([result, blank])
                 displayed += 1
+            updated += 1
+        if not old_data.dropna().equals(new_data.dropna()):
             modified += 1
             modified_old_item = pd.DataFrame([name, 'Old'], index=['NAME', 'BANK'])
             if f != 'D':
@@ -87,12 +96,10 @@ for f in freqlist:
                 modified_new_item = pd.concat([modified_new_item, new_data.iloc[::-1]]).T
             else:
                 modified_new_item = pd.concat([modified_new_item, new_data]).T
-            modified_result = pd.concat([modified_new_item, modified_old_item], ignore_index=True).iloc[::-1].T.dropna(axis=0, how="all")
+            modified_result = pd.concat([ modified_old_item, modified_new_item], ignore_index=True).T.dropna(axis=0, how="all")
             modified_data[f] = pd.concat([modified_data[f], pd.concat([modified_result.loc["NAME"], modified_result[modified_result[0] != modified_result[1]].T], axis=1)])
             # else:
             #     result = pd.concat([result, pd.DataFrame([['modified:', False]])])
-            if displayed <= display_num and len(new_data.dropna()) > len(old_data.dropna()):
-                result = pd.concat([result, blank])
     modified_data[f] = modified_data[f].dropna(axis=1, how="all")
     if f == 'A':
         modified_data[f].columns = [str(col) for col in modified_data[f].columns]
@@ -103,8 +110,10 @@ for f in freqlist:
 sys.stdout.write("\n\n")
 
 print('\nRun Time: '+str(int(time.time() - tStart))+' s'+'\n')
+print("Total Updated items = "+str(updated))
 print("Total Modified items = "+str(modified))
 result = pd.concat([result, blank])
+result = pd.concat([result, pd.DataFrame([['Total Updated items:', updated]])])
 result = pd.concat([result, pd.DataFrame([['Total Modified items:', modified]])])
 
 print('\nOutputing Results, Time: '+str(int(time.time() - tStart))+' s'+'\n')
